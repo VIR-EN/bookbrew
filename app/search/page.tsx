9ae2c6f4ea9f25248ece2ac7a5051f7164c1021e
Deanson Choo <deanson.choo@gmail.com>
2025-12-05 19:47:14 -0400

    basic styling of page.tsx

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -12,6 +12,7 @@ export default function SearchPage() {
     const query = searchParams.get('q'); //Grab the Search Query from the URL
     const [books, setBooks] = useState([]);
     const [loading, setLoading] = useState(true);
+    const[error, setError] = useState(false);
 
     useEffect(() => {
         if (query) {
@@ -19,17 +20,38 @@ export default function SearchPage() {
         }
     }, [query]);
 
+    console.log(books);
+
     const fetchBooks = async (searchQuery: string) => {
-        const response = await fetch(`/api/books?q=${encodeURIComponent(searchQuery)}`); //Calls our API endpoint
-        const data = await response.json();
-        setBooks(data.books);
-        setLoading(false)
+        try {
+            const response = await fetch(`/api/books?q=${encodeURIComponent(searchQuery)}`); //Calls our API endpoint
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const data = await response.json();
+            setBooks(data.books);
+        }
+        catch (error) {
+            console.log(error)
+            setError(true)
+        }
+        finally {
+            setLoading(false)
+        }
     }
 
     if (loading) {
         return (
             <div className = "flex items-center justify-center h-screen">
                 <h1 className = "font-bold text-5xl">Loading...</h1>
+            </div>
+        )
+    }
+
+    if (error) {
+        return (
+            <div className = "flex items-center justify-center h-screen">
+                <h1 className = "text-red-500 font-bold text-5xl">Error Fetching Books</h1>
             </div>
         )
     }
@@ -44,28 +66,42 @@ export default function SearchPage() {
     return (
         <>
             <Header/>
-            <div className="max-w-4xl mx-auto p-6">
-                <div className="space-y-4">
-                    {books.map((book: any) => (
-                        <div key={book.id} className="border border-gray-200 p-4">
-                            <h1 className="text-xl font-semibold text-gray-800">
-                                {book.title}
-                            </h1>
-                            {book.authors.map((author: any) => (
-                            <p key = {author.name} className="text-gray-600 mr-5">By {author.name} </p>
-                            ))}
-                            <Link
-                                href={
-                                `/books/${book.id}/addReview?title=${encodeURIComponent(book.title)}&bookId=${book.id}`}
-                                rel="noopener noreferrer" target="_blank"> Add A Review
-                            </Link>
-                            <Link
-                                href={`/books/${book.id}/reviews?title=${encodeURIComponent(book.title)}`}
-                                className="text-blue-600 border-b mr-5"> View Reviews
-                            </Link>
+            <div className="max-w-4xl mx-auto p-6 space-y-4">
+                {books.map((book: any) => (
+                    <div key={book.id} className="border rounded-md p-2">
+                        <div className = "flex">
+                            <img src = {book.image} alt = {book.title} />
+                            {/* Text Container */}
+                            <div className = "w-full flex flex-col">
+                                <div>
+                                    <h1 className="capitalize text-4xl font-semibold text-gray-800 pl-2">
+                                        {book.title}
+                                    </h1>
+                                    <div className = "pl-2">
+                                        {book.authors.map((author: any) => (
+                                        <p key = {author.name} className="text-xl text-gray-600 mr-5">By {author.name} </p>
+                                        ))}
+                                    </div>
+                                </div>
+
+                                <div className = "pl-2 flex-1 flex flex-col justify-center">
+                                    <Link
+                                        href={
+                                            `/books/${book.id}/addReview?title=${encodeURIComponent(book.title)}&bookId=${book.id}`}
+                                        rel="noopener noreferrer" target="_blank"
+                                        className="bg-gray-500 text-white font-bold border rounded-md mb-2 p-2 h-fit w-fit"
+                                    >Add A Review
+                                    </Link>
+
+                                    <Link
+                                        href={`/books/${book.id}/reviews?title=${encodeURIComponent(book.title)}`}
+                                        className="bg-blue-400 text-white font-bold rounded-md p-2 h-fit w-fit">View Reviews
+                                    </Link>
+                                </div>
+                            </div>
                         </div>
-                    ))}
-                </div>
+                    </div>
+                ))}
             </div>
         </>
     );
