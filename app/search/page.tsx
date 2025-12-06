e94a4e0a01921e69cbcfa8dd0992ee5de153e63b
Erin Cho <echo@Erins-MacBook-Air-2.local>
2025-12-06 11:18:04 -0500

    Fixed errors

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -87,7 +87,7 @@ export default function SearchPage() {
                                 <div className = "pl-2 flex-1 flex flex-col justify-center">
                                     <Link
                                         href={
-                                            `/addReview/?title=${encodeURIComponent(book.title)}&bookId=${book.id}`}
+                                            `/addReview/?title=${encodeURIComponent(book.title)}&bookId=${encodeURI(book.id)}`}
                                         rel="noopener noreferrer" target="_blank"
                                         className="bg-gray-500 text-white font-bold border rounded-md mb-2 p-2 h-fit w-fit"
                                     >Add A Review
